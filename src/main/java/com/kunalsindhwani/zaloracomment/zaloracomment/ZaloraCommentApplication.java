d55d875dfd55925aa9e92202cbfc6df1a415b055
Kunal Sindhwani <kunalsindhwani@hotmail.com>
2019-12-17 02:12:20 +0800

    Add files via upload

---
+++ src/main/java/com/kunalsindhwani/zaloracomment/zaloracomment/ZaloraCommentApplication.java
@@ -0,0 +1,13 @@
+package com.kunalsindhwani.zaloracomment.zaloracomment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaloraCommentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaloraCommentApplication.class, args);
+	}
+
+}
